09093f70245eebc3ecc96975b1277a027a226b89
Brays83 <102981505+Brays83@users.noreply.github.com>
2024-04-25 11:10:34 -0500

    feat(Auth): Add register component

---
+++ src/app/Auth/pages/register/register.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {UserService} from "../../service/user.service";
+import {User} from "../../model/user.entity";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  private user= {} as User;
+  constructor(
+    private formBuilder:FormBuilder,
+    private userService:UserService,
+    private router:Router) {
+  }
+  registerForm:FormGroup = this.formBuilder.group({
+    email: ['',{validators:[Validators.required,Validators.email],updateOn: `change`}],
+    password: ['',{validators: [Validators.required,Validators.maxLength(8)],updateOn: `change`}],
+    name:['',{validators:[Validators.required]}],
+    lastName:['',{validators:[Validators.required]}],
+    confirmPassword:['',{validators:[Validators.required]}],
+    Company:['',{validators:[Validators.required]}],
+    birthday:['',{validators:[Validators.required]}],
+    country:['',{validators:[Validators.required]}],
+    city:['',{validators:[Validators.required]}],
+    rol: [['',{validators:[Validators.required]}]],
+  })
+  onRegister():void{
+    if(this.registerForm.invalid)return;
+    this.user.email= this.registerForm.controls['email'].value
+    this.user.password = this.registerForm.controls['password'].value
+    this.userService.create(this.user).subscribe(response =>{
+      this.router.navigate(["/auth/login"]);
+
+      }
+
+    )
+  }
+}
