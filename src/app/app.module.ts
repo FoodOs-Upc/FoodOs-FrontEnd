f2116ed4b504bcb079fdfcb002e240ec4a3e607a
Alessandro  Vega <83603934+Alessandrovega@users.noreply.github.com>
2024-04-28 18:29:10 -0500

    Creacion componente team
    Creo el apartado para el desarrollo de la Epica 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,15 @@ import {MatListItem, MatNavList} from "@angular/material/list";
 import { LayoutComponent } from './Public/pages/layout/layout.component';
 import {MatSidenav, MatSidenavContainer, MatSidenavModule} from "@angular/material/sidenav";
 import {MatIcon} from "@angular/material/icon";
+import { TeamComponent } from './public/pages/team/team.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomSidenavComponent,
-    LayoutComponent
+    LayoutComponent,
+    TeamComponent
   ],
     imports: [
         BrowserModule,
