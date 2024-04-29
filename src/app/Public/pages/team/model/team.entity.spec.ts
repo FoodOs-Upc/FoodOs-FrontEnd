5c5df0aa666d74d28335eff742a766879cc74868
Alessandro  Vega <83603934+Alessandrovega@users.noreply.github.com>
2024-04-29 13:08:46 -0500

    Funcionando Añadir, Eliminar y Editar
    Funciona en los memebers

--- src/app/Public/pages/team/model/team.entity.spec.ts
+++ src/app/Public/pages/team/model/team.entity.spec.ts
@@ -2,6 +2,7 @@ import { TeamEntity } from './team.entity';
 
 describe('TeamEntity', () => {
   it('should create an instance', () => {
+    // @ts-ignore
     expect(new TeamEntity()).toBeTruthy();
   });
 });
--- src/app/Public/pages/team/team.component.ts
+++ src/app/Public/pages/team/team.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {MatCardModule} from "@angular/material/card";
 import {TeamEntity} from "./model/team.entity";
 import {TeamService} from "./service/team.service";
@@ -6,20 +6,29 @@ import {MatButtonModule} from "@angular/material/button";
 import {CommonModule} from "@angular/common";
 import {MatIcon} from "@angular/material/icon";
 import {MatFormField} from "@angular/material/form-field";
+import {FormsModule} from "@angular/forms";
+import {MatInput} from "@angular/material/input";
+import {log} from "@angular-devkit/build-angular/src/builders/ssr-dev-server";
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
   styleUrl: './team.component.css',
-  imports: [MatCardModule, MatButtonModule, CommonModule, MatIcon, MatFormField],
+  imports: [MatCardModule, MatButtonModule, CommonModule, MatIcon, MatFormField, FormsModule, MatInput],
   standalone: true
 })
 export class TeamComponent implements OnInit {
   teamMembers: TeamEntity[] = [];
+  @Input()member: TeamEntity;
 
+
+
+  @Input() editMode = false;
   constructor(
     private teamService: TeamService
-  ) { }
+  ) {
+    this.member ={} as TeamEntity;
+  }
 
   ngOnInit(): void {
     this.getTeamData();
@@ -28,7 +37,6 @@ export class TeamComponent implements OnInit {
   getTeamData(): void {
     this.teamService.getAll().subscribe((res: any) => {
       this.teamMembers = res;
-      console.log(res)
     });
   }
   deleteMember(id: any): void {
@@ -36,7 +44,36 @@ export class TeamComponent implements OnInit {
       this.getTeamData();
     });
   }
+  onSubmit() {
+      if (this.editMode) {
+        this.teamService.update(this.member.id, this.member).subscribe({
+          next: () => {
+            this.teamService.getAll().subscribe((res: any) => {
+              this.teamMembers = res;
+              this.member = {} as TeamEntity;
+              this.editMode = false;
+            });
+          }
+        });
+      } else {
+        this.teamService.create(this.member).subscribe({
+          next: (response: any) => {
+            this.teamMembers.push(response);
+            this.member = {} as TeamEntity;
+          }
+        });
+      }
+  }
 
+  onCancel() {
+    this.member = {} as TeamEntity;
+    this.editMode = false;
+  }
+
+  edit(member: TeamEntity) {
+    this.editMode = true;
+    this.member = {...member};
+  }
 
   //Delete ejemplo
   /*
