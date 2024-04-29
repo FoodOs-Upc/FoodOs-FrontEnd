d2d41bc45381468fa88390ca9d9a79c443801a50
insomnio <max9347@hotmail.com>
2024-04-29 17:58:44 -0500

    add contructors for components

--- src/app/suppliers-edit/suppliers-edit.component.ts
+++ src/app/suppliers-edit/suppliers-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-suppliers-edit',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './suppliers-edit.component.css'
 })
 export class SuppliersEditComponent {
-
+  suppliersForm: FormGroup;
+cook:string[] = [
+  'Add export',
+  'Add task to cheff ',
+]
+  constructor(private  _fb:FormBuilder) {
+  this.suppliersForm = this._fb.group()
+  }
 }
